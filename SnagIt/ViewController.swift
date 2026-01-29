b079718288b33bd2ba5327b3bcef3f39d352721a
mishacausur <mishacausur@mail.ru>
2026-01-29 13:48:39 +0500

    Initial Commit

---
+++ SnagIt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnagIt
+//
+//  Created by Misha Causur on 29.01.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
